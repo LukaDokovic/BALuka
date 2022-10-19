41207c4cb5381c648b71f0121384a337219b353c
LukaDokovic <dokovic68@gmail.com>
2022-10-19 15:46:38 +0200

    19.10.2022

--- Kringel.py
+++ Kringel.py
@@ -6,7 +6,7 @@ import sys
 import math
 import os
 
-
+Spandau = gmsh.model.occ
 gmsh.initialize()
 
 
@@ -81,61 +81,70 @@ index4list=[]
 
 #über jede Koordinatenpackung iterieren
 for [x,y,z] in (coords[:-1]):
-        gmsh.model.occ.addPoint(x, y, z, lc, index1)
+        Spandau.addPoint(x, y, z, lc, index1)
         index1list.append(index1)
         index1+=1
-        
+"""
 for i in range(Anzahl-1):
-    gmsh.model.occ.addSpline([index1-1,index1-2])
+    Spandau.addSpline([index1-1,index1-2])
     index1-=1
-
-#gmsh.model.occ.addSpline(index1list)
+"""
+print("index1list = ",index1list)
+Spandau.addBSpline(index1list, degree=2)
 
 
 #dem ganzen ding eine Breite geben
 for [x,y,b] in (coordsb[:-1]):
-        gmsh.model.occ.addPoint(x, y, b, lc, index3)
+        Spandau.addPoint(x, y, b, lc, index3)
         index3list.append(index3)
         index3+=1
-
-for i in range(Anzahl-1):
-    gmsh.model.occ.addSpline([index3-1,index3-2])
-    index3-=1
-      
-#gmsh.model.occ.addSpline(index3list)
+    
+Spandau.addBSpline(index3list, degree=2)
 
 for [xc,yc,z] in coordsd:
-        gmsh.model.occ.addPoint(xc, yc, z, lc, index2)
+        Spandau.addPoint(xc, yc, z, lc, index2)
         index2list.append(index2)
         index2+=1
 
-for i in range(Anzahl-1):
-    gmsh.model.occ.addSpline([index2-1,index2-2])
-    index2-=1
-
-#gmsh.model.occ.addSpline(index2list)
+Spandau.addBSpline(index2list, degree=2)
 
 for [xc,yc,b] in coordsdb:
-        gmsh.model.occ.addPoint(xc, yc, b, lc, index4)
+        Spandau.addPoint(xc, yc, b, lc, index4)
         index4list.append(index4)
         index4+=1
-
-for i in range(Anzahl-1):
-    gmsh.model.occ.addSpline([index4-1,index4-2])
-    index4-=1
-  
-#gmsh.model.occ.addSpline(index4list)
-
+ 
+Spandau.addBSpline(index4list,degree=2)
   
 
 #Punktwolke verbinden
-gmsh.model.occ.addSpline([index1-1,index2-1])
-gmsh.model.occ.addSpline([index2-1,index4-1])
-gmsh.model.occ.addSpline([index3-1,index4-1])
-gmsh.model.occ.addSpline([index3-1,index1-1])
+Spandau.addSpline([index1-1,index2-1])
+Spandau.addSpline([index2-1,index4-1])
+Spandau.addSpline([index3-1,index4-1])
+Spandau.addSpline([index3-1,index1-1])
 
 
-gmsh.model.occ.synchronize()
+#fester Span 
+Spandau.addPoint(dicke,0,0,lc,70)
+Spandau.addPoint(dicke,0,b,lc,71)
+Spandau.addPoint(0,0,b,lc,72)
+Spandau.addLine(70,71)
+Spandau.addLine(71,72)
+Spandau.addPoint(1,2.5,b,lc,73)
+
+norm2 = 1/np.sqrt(1+np.square(2.5))
+Spanx = dicke * (-2.5*norm2)
+Spany = dicke * (1*norm2)
+Spandau.addPoint(1-Spanx,2.5-Spany,0,lc,74)
+Spandau.addPoint(1-Spanx,2.5-Spany,b,lc,75)
+
+abstand = (np.sqrt(np.square(resultx[0]-1)+np.square(resulty[0]-2.5)))/2
+vektorx = norm2*abstand
+vektory = norm2*abstand*2.5
+Spandau.addPoint(1+vektorx, 2.5+vektory,0,lc,76)
+Spandau.addPoint(1+vektorx, 2.5+vektory,b,lc,77)
+
+
+Spandau.synchronize()
 gmsh.model.mesh.generate(1)
 gmsh.write("Kringel.msh")
 gmsh.fltk.run()
