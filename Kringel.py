c217469f1a43118fb169f7bb55d2497cba43fb1a
LukaDokovic <dokovic68@gmail.com>
2022-10-20 14:57:55 +0200

    20.10.2022

--- Kringel.py
+++ Kringel.py
@@ -1,23 +1,23 @@
 import numpy as np
 from scipy.special import fresnel
-import matplotlib.pyplot as plt
 import gmsh
 import sys
 import math
 import os
-
 Spandau = gmsh.model.occ
 gmsh.initialize()
-
-
 gmsh.open('./Analogieprüfstand_Spanraum.STEP')
 
 Zoom = 70
 Anzahl = 50
-t = np.linspace(0.38, 3, Anzahl+1) #erzeugt 5 Punkte (letzter Eintrag) von 0-5 (ersten zwei Einträge)
+Start = 0.38
+Ende = 3
+xVerschiebung = -7.47
+yVerschiebung = -38
+t = np.linspace(Start, Ende, Anzahl+1) #erzeugt 5 Punkte (letzter Eintrag) von 0-5 (ersten zwei Einträge)
 sin, cos = np.sqrt(fresnel(t)) #fresnelintegral erzeugen
-x = ((sin)*Zoom)-7.46
-y = ((cos)*Zoom)-38
+x = ((sin)*Zoom)+xVerschiebung
+y = ((cos)*Zoom)+yVerschiebung
 z = t * 0 #verschiebung in z
 b = z - 23 #breite
 dicke = 1
@@ -28,8 +28,9 @@ resultdy = []
 
 #Funktion für die Normalenvektoren
 for idx in range(len(x)-1):
-    x0, y0, xn, yn = x[idx], y[idx], x[idx+1], y[idx+1]
-    dx, dy = xn-x0, yn-y0
+    x0, y0, x1, y1 = x[idx], y[idx], x[idx+1], y[idx+1]
+    dx = x1-x0
+    dy = y1-y0
     norm = math.hypot(dx, dy) * 1/dicke
     dx /= norm
     dy /= norm
@@ -89,7 +90,6 @@ for i in range(Anzahl-1):
     Spandau.addSpline([index1-1,index1-2])
     index1-=1
 """
-print("index1list = ",index1list)
 Spandau.addBSpline(index1list, degree=2)
 
 
@@ -123,6 +123,35 @@ Spandau.addSpline([index3-1,index4-1])
 Spandau.addSpline([index3-1,index1-1])
 
 
+
+
+#Vom Span aus Rückwärts
+tSpan1 = Start - 0.01
+sinSpan1, cosSpan1 = np.sqrt(fresnel(tSpan1)) #fresnelintegral erzeugen
+xSpan1 = ((sinSpan1)*Zoom)+xVerschiebung
+ySpan1 = ((cosSpan1)*Zoom)+yVerschiebung
+Spandau.addPoint(xSpan1,ySpan1,0,lc,80)
+Spandau.addPoint(xSpan1,ySpan1,b,lc,81)
+
+#Normalenvektor
+tSpan = np.linspace(Start - 0.01, Start, 2)
+sinSpan, cosSpan = np.sqrt(fresnel(tSpan)) #fresnelintegral erzeugen
+xSpan = ((sinSpan)*Zoom)+xVerschiebung
+ySpan = ((cosSpan)*Zoom)+yVerschiebung
+
+for idx in range(len(xSpan)-1):
+    x0Span, y0Span, x1Span, y1Span = xSpan[idx], ySpan[idx], xSpan[idx+1], ySpan[idx+1]
+    dxSpan = x1Span-x0Span
+    dySpan = y1Span-y0Span
+    normSpan = math.hypot(dxSpan, dySpan) * 1/dicke
+    dxSpan /= normSpan
+    dySpan /= normSpan
+    xcSpan = x0Span-dySpan
+    ycSpan = y0Span+dxSpan
+    Spandau.addPoint(xcSpan,ycSpan,0,lc,82)
+    Spandau.addPoint(xcSpan,ycSpan,b,lc,83)
+
+
 #fester Span 
 Spandau.addPoint(dicke,0,0,lc,70)
 Spandau.addPoint(dicke,0,b,lc,71)
@@ -137,12 +166,18 @@ Spany = dicke * (1*norm2)
 Spandau.addPoint(1-Spanx,2.5-Spany,0,lc,74)
 Spandau.addPoint(1-Spanx,2.5-Spany,b,lc,75)
 
-abstand = (np.sqrt(np.square(resultx[0]-1)+np.square(resulty[0]-2.5)))/2
-vektorx = norm2*abstand
-vektory = norm2*abstand*2.5
+abstand = (np.sqrt(np.square(xcSpan-1)+np.square(ycSpan-2.5)))
+vektorx = norm2*(abstand/4)
+vektory = norm2*(abstand/4)*2.5
 Spandau.addPoint(1+vektorx, 2.5+vektory,0,lc,76)
 Spandau.addPoint(1+vektorx, 2.5+vektory,b,lc,77)
+Spandau.addPoint((1+vektorx)-Spanx, (2.5+vektory)-Spany,0,lc,78)
+Spandau.addPoint((1+vektorx)-Spanx, (2.5+vektory)-Spany,b,lc,79)
 
+Spandau.addBSpline([70,74,78,80,1000],degree=3)
+Spandau.addBSpline([71,75,79,81,3000],degree=3)
+Spandau.addBSpline([1,76,82,2000],degree=3)
+Spandau.addBSpline([73,77,83,4000],degree=3)
 
 Spandau.synchronize()
 gmsh.model.mesh.generate(1)
